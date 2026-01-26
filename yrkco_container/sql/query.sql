5a8fcedf5c9da12593fc12b0539bd6b345743b07
GrannyApples <anton.engstrom1994@gmail.com>
2026-01-26 23:23:27 +0100

    add:query.sql -  program name query

--- yrkco_container/sql/query.sql
+++ yrkco_container/sql/query.sql
@@ -1,3 +1,5 @@
+
+-- 3NF relation
 SELECT u.firstname || ' ' || u.lastname AS utbildare,
        f.name AS foretag,
        a.street,
@@ -7,3 +9,14 @@ FROM utbildare u
 JOIN foretag f ON u.foretag_id = f.id
 JOIN adress a ON f.adress_id = a.id
 JOIN country c ON a.country_id = c.id;
+
+
+-- specifik search for program name
+SELECT p.name AS program,
+       k.name AS kurs,
+       k.coursecode,
+       k.points
+FROM program p
+JOIN programkurs pk ON p.id = pk.program_id
+JOIN kurs k ON pk.kurs_id = k.id
+WHERE p.name = 'Systemutvecklare .NET';
